13550937fb0f376cd691ae9ab5dcf008243922dc
Uzair-uzzi <uzair.aslam89247@gmail.com>
2025-04-17 18:47:23 +0500

    Add files via upload

---
+++ Task4-Take-Value-From-User/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class UserInputExample
+{
+    static void Main()
+    {
+        // Read a string
+        Console.Write("Enter your name: ");
+        //Here we are taking value from user:
+        string name = Console.ReadLine();
+
+        // Read an integer
+        Console.Write("Enter your age: ");
+        //Here we are taking value from user:
+        int age = int.Parse(Console.ReadLine());
+
+        // Output the values
+        Console.WriteLine();
+        Console.WriteLine("Here is your entered information");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+    }
+}
